835d375a55ed920018c876a7682560d5bf86244f
Shiven Saini <shivensaini73@gmail.com>
2024-06-21 15:01:49 +0530

    Working condition

---
+++ app/src/main/java/com/demo/composebasics/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.demo.composebasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.demo.composebasics.ui.theme.ComposeBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBasicsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val text1: String = stringResource(R.string.text1_content)
+                    val text2: String = stringResource(R.string.text2_content)
+                    val text3: String = stringResource(R.string.text3_content)
+                    HomePagelayout(text1 = text1, text2 = text2, text3 = text3)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomePagelayout(text1: String, text2: String, text3: String, modifier: Modifier = Modifier) {
+    val imageRender = painterResource(id = R.drawable.bg_compose_background)
+    Column(modifier=modifier,
+        verticalArrangement = Arrangement.Top,
+    ) {
+        Image(
+            painter = imageRender,
+            contentDescription = stringResource(R.string.jetpack_compose_banner),
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        TextLayout(text1, text2, text3)
+    }
+
+}
+
+@Composable
+fun TextLayout(text1: String, text2: String, text3: String, modifier: Modifier = Modifier) {
+    Column (modifier = Modifier)
+    {
+        Text(
+            text = text1,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+
+        Text(
+            text = text2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+
+        Text(
+            text = text3,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
+}
