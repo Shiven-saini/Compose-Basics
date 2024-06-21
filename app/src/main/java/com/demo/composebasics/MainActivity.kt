16a552d2aee6afbf51f3017f671a1bbf4e7bd544
Shiven Saini <shivensaini73@gmail.com>
2024-06-21 16:11:24 +0530

    done assignment-2

--- app/src/main/java/com/demo/composebasics/MainActivity.kt
+++ app/src/main/java/com/demo/composebasics/MainActivity.kt
@@ -3,20 +3,21 @@ package com.demo.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.demo.composebasics.ui.theme.ComposeBasicsTheme
@@ -24,14 +25,14 @@ import com.demo.composebasics.ui.theme.ComposeBasicsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             ComposeBasicsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val text1: String = stringResource(R.string.text1_content)
-                    val text2: String = stringResource(R.string.text2_content)
-                    val text3: String = stringResource(R.string.text3_content)
-                    val imageRender = painterResource(id = R.drawable.bg_compose_background)
-                    HomePagelayout(text1 = text1, text2 = text2, text3 = text3, imageDraw = imageRender)
+                    val text1: String = stringResource(R.string.all_tasks_completed)
+                    val text2: String = stringResource(R.string.nice_work)
+                    val imageRender = painterResource(id = R.drawable.ic_task_completed)
+                    TaskCompleted(text1 = text1, text2 = text2, imageDraw = imageRender)
                 }
             }
         }
@@ -39,33 +40,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomePagelayout(text1: String, text2: String, text3: String, imageDraw: Painter, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.Top,
-    ) {
+fun TaskCompleted(text1: String, text2: String, imageDraw: Painter, modifier: Modifier = Modifier) {
+
+    Column (modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
         Image(
             painter = imageDraw,
-            contentDescription = stringResource(R.string.jetpack_compose_banner),
-            modifier = Modifier.fillMaxWidth()
+            contentDescription = null
         )
-
         Text(
             text = text1,
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
         )
-
         Text(
             text = text2,
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
-        )
-
-        Text(
-            text = text3,
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(16.dp)
+            fontSize = 16.sp
         )
     }
 }
+
+
